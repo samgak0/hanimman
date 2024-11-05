37931a91b3f43d3a6a39a2bad13b17052b5d3638
p614080 <p614080@naver.com>
2024-11-05 16:25:24 +0900

    share 엔티티 추가

---
+++ src/main/java/org/devkirby/hanimman/entity/share.java
@@ -0,0 +1,35 @@
+package org.devkirby.hanimman.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Table(name="share")
+public class share {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+    private String content;
+    private int views;
+    private Timestamp createAt;
+    private Timestamp modifiedAt;
+    private Timestamp deletedAt;
+    private String location;
+    private Timestamp locationDate;
+    private String item;
+    private int quantity;
+    private boolean status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private int memberId;
+}
