2c06d17927d9dcc4e9f1756f29bf8dc5637765a5
samgak <183370871+samgak0@users.noreply.github.com>
2024-11-06 18:17:03 +0900

    미사용 import 제거

--- src/main/java/org/devkirby/hanimman/repository/ShareParticipantRepository.java
+++ src/main/java/org/devkirby/hanimman/repository/ShareParticipantRepository.java
@@ -1,12 +1,11 @@
 package org.devkirby.hanimman.repository;
 
+import java.util.List;
+
 import org.devkirby.hanimman.entity.ShareParticipant;
 import org.devkirby.hanimman.entity.Together;
-import org.devkirby.hanimman.entity.TogetherParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ShareParticipantRepository extends JpaRepository<ShareParticipant, Integer> {
