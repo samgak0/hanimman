ff12aba8998fa34a3a1a4e7fff08413d2b3e734d
sonamuyaz <musiwngud@naver.com>
2024-11-06 17:14:46 +0900

    Repository명 수정중

--- src/main/java/org/devkirby/hanimman/repository/ChatParticipantRepository.java
+++ src/main/java/org/devkirby/hanimman/repository/ChatParticipantRepository.java
@@ -1,10 +1,10 @@
 package org.devkirby.hanimman.repository;
 
-import org.devkirby.hanimman.entity.ChatParticipants;
+import org.devkirby.hanimman.entity.ChatParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChatParticipantsRepository extends JpaRepository<ChatParticipants, Integer> {
+public interface ChatParticipantRepository extends JpaRepository<ChatParticipant, Integer> {
     
 }
--- src/main/java/org/devkirby/hanimman/repository/ChatRoomRepository.java
+++ src/main/java/org/devkirby/hanimman/repository/ChatRoomRepository.java
@@ -1,10 +1,10 @@
 package org.devkirby.hanimman.repository;
 
-import org.devkirby.hanimman.entity.ChatRooms;
+import org.devkirby.hanimman.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChatRoomsRepository extends JpaRepository<ChatRooms, Integer> {
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Integer> {
     
 }
