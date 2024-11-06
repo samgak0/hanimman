33aff3ce26ae24998612b9aa3f2ef6e0e5075e36
sonamuyaz <musiwngud@naver.com>
2024-11-06 17:36:53 +0900

    Repository명 수정중

--- src/main/java/org/devkirby/hanimman/repository/TogetherReportsRepository.java
+++ src/main/java/org/devkirby/hanimman/repository/TogetherReportsRepository.java
@@ -1,9 +1,9 @@
 package org.devkirby.hanimman.repository;
 
-import org.devkirby.hanimman.entity.ShareReport;
+import org.devkirby.hanimman.entity.TogetherReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TogetherReportsRepository extends JpaRepository<ShareReport, Integer> {
+public interface TogetherReportsRepository extends JpaRepository<TogetherReport, Integer> {
 }
