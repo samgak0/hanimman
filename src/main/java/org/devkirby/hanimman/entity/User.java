381989b2024250778bcd1b0d60069ded1c54780b
sonamuyaz <musiwngud@naver.com>
2024-11-06 11:00:43 +0900

    Gender 컬럼 수정

--- src/main/java/org/devkirby/hanimman/entity/User.java
+++ src/main/java/org/devkirby/hanimman/entity/User.java
@@ -3,12 +3,7 @@ package org.devkirby.hanimman.entity;
 import java.time.LocalDateTime;
 import java.util.Date;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
