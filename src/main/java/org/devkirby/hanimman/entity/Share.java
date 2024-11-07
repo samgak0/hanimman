44e60277fa8d75c1892b9f4f7def259adb5ebb0e
nechya00 <nechya00@gmail.com>
2024-11-07 10:05:34 +0900

    Share.java -- Content의 데이터 타입을 text로 변경 지정하였습니다.

--- src/main/java/org/devkirby/hanimman/entity/Share.java
+++ src/main/java/org/devkirby/hanimman/entity/Share.java
@@ -19,7 +19,7 @@ public class Share {
     @Column(length = 255, nullable = false)
     private String title;
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
 
     @Column(nullable = false)
