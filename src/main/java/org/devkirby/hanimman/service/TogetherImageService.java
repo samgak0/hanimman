23635428e4d004286a9d22c0739c086ea62362ba
p614080 <p614080@naver.com>
2024-11-07 16:58:53 +0900

    TogetherImageService 이미지업로드 추가

--- src/main/java/org/devkirby/hanimman/service/TogetherImageService.java
+++ src/main/java/org/devkirby/hanimman/service/TogetherImageService.java
@@ -1,10 +1,15 @@
 package org.devkirby.hanimman.service;
 
 import org.devkirby.hanimman.dto.TogetherImageDTO;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 public interface TogetherImageService {
     void create(TogetherImageDTO togetherImageDTO);
     TogetherImageDTO read(Integer id);
     void update(TogetherImageDTO togetherImageDTO);
     void delete(Integer id);
+
+    String uploadImage(MultipartFile file, Integer togetherId) throws IOException;
 }
