b6484ad081ee44047d8bed8a470abd100e9192aa
p614080 <p614080@naver.com>
2024-11-07 14:10:23 +0900

    ShareImageService 파일업로드 추가

--- src/main/java/org/devkirby/hanimman/service/ShareImageServiceImpl.java
+++ src/main/java/org/devkirby/hanimman/service/ShareImageServiceImpl.java
@@ -2,17 +2,32 @@ package org.devkirby.hanimman.service;
 
 import lombok.RequiredArgsConstructor;
 import org.devkirby.hanimman.dto.ShareImageDTO;
+import org.devkirby.hanimman.entity.Share;
 import org.devkirby.hanimman.entity.ShareImage;
 import org.devkirby.hanimman.repository.ShareImageRepository;
+import org.devkirby.hanimman.repository.ShareRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.time.Instant;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class ShareImageServiceImpl implements ShareImageService{
+    @Autowired
     private final ShareImageRepository shareImageRepository;
+
+    @Autowired
+    private ShareRepository shareRepository;
+
     private final ModelMapper modelMapper;
 
     @Override
@@ -39,5 +54,36 @@ public class ShareImageServiceImpl implements ShareImageService{
         ShareImage shareImage = shareImageRepository.findById(id).orElseThrow();
         shareImage.setDeletedAt(Instant.now());
         shareImageRepository.save(shareImage);
+    }
+
+    @Override
+    public String uploadImage(MultipartFile file, Integer shareId) throws IOException {
+        String uploadDir = "shareImages/";
+        File uploadFile = new File(uploadDir);
+        if(!uploadFile.exists()) {
+            uploadFile.mkdirs();
+        }
+
+        String originalName = file.getOriginalFilename();
+        String serverName = UUID.randomUUID().toString() + "_" + originalName;
+
+        Path targetPath = new File(uploadDir + serverName).toPath();
+
+        Files.copy(file.getInputStream(), targetPath, StandardCopyOption.REPLACE_EXISTING);
+
+        Share share = shareRepository.findById(shareId).orElseThrow();
+
+        ShareImage shareImage = ShareImage.builder()
+                .originalName(originalName)
+                .serverName(serverName)
+                .mineType(file.getContentType())
+                .fileSize((int) file.getSize())
+                .parent(share)
+                .user(share.getUser())
+                .build();
+
+        shareImageRepository.save(shareImage);
+
+        return "File uploaded successfully" + serverName;
     }
 }
