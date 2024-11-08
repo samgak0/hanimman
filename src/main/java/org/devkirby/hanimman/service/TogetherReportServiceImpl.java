1f09ce436f279060f8ef76c601fea43c167d9591
p614080 <p614080@naver.com>
2024-11-08 11:31:56 +0900

    Report 추가

---
+++ src/main/java/org/devkirby/hanimman/service/TogetherReportServiceImpl.java
@@ -0,0 +1,42 @@
+package org.devkirby.hanimman.service;
+
+import lombok.RequiredArgsConstructor;
+import org.devkirby.hanimman.entity.ReportCategory;
+import org.devkirby.hanimman.entity.Together;
+import org.devkirby.hanimman.entity.TogetherReport;
+import org.devkirby.hanimman.entity.User;
+import org.devkirby.hanimman.repository.ReportCategoryRepository;
+import org.devkirby.hanimman.repository.TogetherReportRepository;
+import org.devkirby.hanimman.repository.TogetherRepository;
+import org.devkirby.hanimman.repository.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TogetherReportServiceImpl implements TogetherReportService {
+    private final TogetherReportRepository togetherReportRepository;
+    private final ModelMapper modelMapper;
+    private final UserRepository userRepository;
+    private final TogetherRepository togetherRepository;
+    private final ReportCategoryRepository reportCategoryRepository;
+
+    @Override
+    public void create(Integer reporterId, Integer categoryId, Integer parentId) {
+        Together together = togetherRepository.findById(parentId).orElseThrow();
+        User reportedUser = together.getUser();
+        User reporterUser = userRepository.findById(reporterId).orElseThrow();
+        ReportCategory category = reportCategoryRepository.findById(categoryId).orElseThrow();
+        togetherReportRepository.save(TogetherReport.builder()
+                .reporter(reporterUser)
+                .reported(reportedUser)
+                .category(category)
+                .parent(together)
+                .build());
+    }
+
+    @Override
+    public void delete(Integer id) {
+        togetherReportRepository.deleteById(id);
+    }
+}
