8fba1480893e1397eca4ea15bbbb7d3380c7d0ec
sonamuyaz <musiwngud@naver.com>
2024-11-08 18:08:54 +0900

    ProfileService 생성 test완료

--- src/main/java/org/devkirby/hanimman/config/SecurityConfig.java
+++ src/main/java/org/devkirby/hanimman/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authorizeRequests) ->
                         authorizeRequests
 //                                .requestMatchers(PathRequest.toH2Console()).permitAll()
-                                .requestMatchers("/*/**").permitAll()
+                                .requestMatchers("/*/**").permitAll() // 모든 정적 리소스 접근 허용
                                 .anyRequest().authenticated()
                 );
         return http.build();
---
+++ src/main/java/org/devkirby/hanimman/service/ProfileService.java
@@ -0,0 +1,54 @@
+package org.devkirby.hanimman.service;
+
+import lombok.Setter;
+import org.devkirby.hanimman.entity.Profile;
+import org.devkirby.hanimman.repository.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Service
+public class ProfileService {
+
+    @Autowired
+    private ProfileRepository profileRepository; // 프로필 저장소
+
+    // Setter 메서드 추가
+    @Value("${com.devkirby.hanimman.upload}")
+    @Setter
+    private String uploadDir; // 파일 저장 디렉토리
+
+    public Profile uploadProfilePicture(MultipartFile file, Profile profile) throws IOException {
+        // 파일 이름 가져오기
+        String originalFileName = file.getOriginalFilename();
+
+        // UUID 생성
+        String uuid = UUID.randomUUID().toString(); // UUID 생성
+
+        // serverName에 파일 이름과 UUID 결합
+        String fileNameWithUUID = uuid + '-' + originalFileName;
+        profile.setServerName(fileNameWithUUID);
+
+        // 파일 경로 설정
+        Path filePath = Paths.get(uploadDir + fileNameWithUUID);
+        Path fileDir = Paths.get(uploadDir);
+
+        // 파일 저장
+        file.transferTo(filePath.toFile());
+
+        // 프로필 정보 업데이트
+        profile.setFileSize((int) file.getSize());
+        profile.setMineType(file.getContentType());
+        profile.setRealName(originalFileName); // 원래 파일 이름
+
+        // 프로필 저장
+        return profileRepository.save(profile);
+    }
+}
