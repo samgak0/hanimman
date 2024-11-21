197d05d6d36f7f6f398efacbabbe4ae5c0746938
p614080 <p614080@naver.com>
2024-11-21 17:55:47 +0900

    Participant controller 오류 수정

--- src/main/java/org/devkirby/hanimman/controller/ShareParticipantController.java
+++ src/main/java/org/devkirby/hanimman/controller/ShareParticipantController.java
@@ -11,40 +11,40 @@ import java.util.List;
 @RequestMapping("/share-participant")
 @RequiredArgsConstructor
 public class ShareParticipantController {
-//    private final ShareParticipantService shareParticipantService;
-//
-//    @PostMapping
-//    public void createParticipant(ShareParticipantDTO shareParticipantDTO) {
-//        shareParticipantService.create(shareParticipantDTO);
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ShareParticipantDTO readParticipant(Integer id) {
-//        return shareParticipantService.read(id);
-//    }
-//
-//    @PutMapping("/{id}")
-//    public void updateParticipant(ShareParticipantDTO shareParticipantDTO) {
-//        shareParticipantService.update(shareParticipantDTO);
-//    }
-//
-//    @PutMapping("/{id}")
-//    public void rejectedParticipant(Integer id) {
-//        shareParticipantService.rejected(id);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public void deleteParticipant(Integer id) {
-//        shareParticipantService.delete(id);
-//    }
-//
-//    @GetMapping("/list/{parentId}")
-//    public List<ShareParticipantDTO> listAllByParentId(Integer parentId) {
-//        return shareParticipantService.listAllByParentId(parentId);
-//    }
-//
-//    @GetMapping("/list/{parentId}/rejected-false")
-//    public List<ShareParticipantDTO> listAllByParentIdAndRejectedIsFalse(Integer parentId) {
-//        return shareParticipantService.listAllByParentIdAndRejectedIsFalse(parentId);
-//    }
+    private final ShareParticipantService shareParticipantService;
+
+    @PostMapping
+    public void createParticipant(ShareParticipantDTO shareParticipantDTO) {
+        shareParticipantService.create(shareParticipantDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ShareParticipantDTO readParticipant(Integer id) {
+        return shareParticipantService.read(id);
+    }
+
+    @PutMapping("/{id}")
+    public void updateParticipant(ShareParticipantDTO shareParticipantDTO) {
+        shareParticipantService.update(shareParticipantDTO);
+    }
+
+    @PutMapping("/{id}/rejected")
+    public void rejectedParticipant(Integer id) {
+        shareParticipantService.rejected(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteParticipant(Integer id) {
+        shareParticipantService.delete(id);
+    }
+
+    @GetMapping("/list/{parentId}")
+    public List<ShareParticipantDTO> listAllByParentId(Integer parentId) {
+        return shareParticipantService.listAllByParentId(parentId);
+    }
+
+    @GetMapping("/list/{parentId}/rejected-false")
+    public List<ShareParticipantDTO> listAllByParentIdAndRejectedIsFalse(Integer parentId) {
+        return shareParticipantService.listAllByParentIdAndRejectedIsFalse(parentId);
+    }
 }
--- src/main/java/org/devkirby/hanimman/controller/TogetherParticipantController.java
+++ src/main/java/org/devkirby/hanimman/controller/TogetherParticipantController.java
@@ -11,40 +11,40 @@ import java.util.List;
 @RequestMapping("/together-participant")
 @RequiredArgsConstructor
 public class TogetherParticipantController {
-//    private final TogetherParticipantService togetherParticipantService;
-//
-//    @PostMapping
-//    public void createParticipant(TogetherParticipantDTO togetherParticipantDTO) {
-//        togetherParticipantService.create(togetherParticipantDTO);
-//    }
-//
-//    @GetMapping("/{id}")
-//    public TogetherParticipantDTO readParticipant(Integer id) {
-//        return togetherParticipantService.read(id);
-//    }
-//
-//    @PutMapping("/{id}")
-//    public void updateParticipant(TogetherParticipantDTO togetherParticipantDTO) {
-//        togetherParticipantService.update(togetherParticipantDTO);
-//    }
-//
-//    @PutMapping("/{id}")
-//    public void rejectedParticipant(Integer id) {
-//        togetherParticipantService.rejected(id);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public void deleteParticipant(Integer id) {
-//        togetherParticipantService.delete(id);
-//    }
-//
-//    @GetMapping("/list/{parentId}")
-//    public List<TogetherParticipantDTO> listAllByParentId(Integer parentId) {
-//        return togetherParticipantService.listAllByParentId(parentId);
-//    }
-//
-//    @GetMapping("/list/{parentId}/rejected-false")
-//    public List<TogetherParticipantDTO> listAllByParentIdAndRejectedIsFalse(Integer parentId) {
-//        return togetherParticipantService.listAllByParentIdAndRejectedIsFalse(parentId);
-//    }
+    private final TogetherParticipantService togetherParticipantService;
+
+    @PostMapping
+    public void createParticipant(TogetherParticipantDTO togetherParticipantDTO) {
+        togetherParticipantService.create(togetherParticipantDTO);
+    }
+
+    @GetMapping("/{id}")
+    public TogetherParticipantDTO readParticipant(Integer id) {
+        return togetherParticipantService.read(id);
+    }
+
+    @PutMapping("/{id}")
+    public void updateParticipant(TogetherParticipantDTO togetherParticipantDTO) {
+        togetherParticipantService.update(togetherParticipantDTO);
+    }
+
+    @PutMapping("/{id}/rejected")
+    public void rejectedParticipant(Integer id) {
+        togetherParticipantService.rejected(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteParticipant(Integer id) {
+        togetherParticipantService.delete(id);
+    }
+
+    @GetMapping("/list/{parentId}")
+    public List<TogetherParticipantDTO> listAllByParentId(Integer parentId) {
+        return togetherParticipantService.listAllByParentId(parentId);
+    }
+
+    @GetMapping("/list/{parentId}/rejected-false")
+    public List<TogetherParticipantDTO> listAllByParentIdAndRejectedIsFalse(Integer parentId) {
+        return togetherParticipantService.listAllByParentIdAndRejectedIsFalse(parentId);
+    }
 }
