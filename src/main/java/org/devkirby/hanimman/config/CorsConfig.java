6a18c09ceb338ae045235df061387150c4b157d4
nechya00 <122517224+Saeng-ah@users.noreply.github.com>
2024-11-28 10:10:31 +0900

    CorsConfig 추가 및 아직 로그인 문제

---
+++ src/main/java/org/devkirby/hanimman/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package org.devkirby.hanimman.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        System.out.println("필터");
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // 자격 증명 허용 (쿠키, 인증 헤더 등)
+        config.addAllowedOriginPattern("*"); // 모든 출처 허용 (필요에 따라 변경 가능)
+        config.addAllowedHeader("*"); // 모든 헤더 허용
+        config.addAllowedMethod("*"); // 모든 HTTP 메서드 허용 (GET, POST, PUT 등)
+        config.addExposedHeader("Authorization"); // 클라이언트가 읽을 수 있는 헤더
+        config.addExposedHeader("Refresh-Token");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // 모든 경로에 대해 설정 적용
+        return new CorsFilter(source);
+    }
+}
