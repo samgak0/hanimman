d50e4cae9752e971c1dbe21f9344b4a32c5147c9
nechya00 <122517224+Saeng-ah@users.noreply.github.com>
2024-11-29 11:02:29 +0900

    로그인 및 로그인 했을 때 로그인 정보 가져오기 성공

---
+++ src/main/java/org/devkirby/hanimman/controller/MainController.java
@@ -0,0 +1,20 @@
+package org.devkirby.hanimman.controller;
+
+import org.devkirby.hanimman.config.CustomUserDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/main")
+public class MainController {
+
+    @GetMapping
+    public void main(@AuthenticationPrincipal CustomUserDetails loginUser){
+        System.out.println("-------------------------------메인 컨트롤러");
+        System.out.println(loginUser);
+        System.out.println(loginUser.toString());
+
+    }
+}
