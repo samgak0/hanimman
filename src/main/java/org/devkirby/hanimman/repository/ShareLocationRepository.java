80f4e0b18c7a0d2ef90ad93fe9a8548d0bad834d
p614080 <p614080@naver.com>
2024-12-03 19:14:03 +0900

    share, together 테이블에 Location 테이블 분리

--- src/main/java/org/devkirby/hanimman/repository/ShareLocationRepository.java
+++ src/main/java/org/devkirby/hanimman/repository/ShareLocationRepository.java
@@ -1,8 +1,9 @@
 package org.devkirby.hanimman.repository;
 
+import org.devkirby.hanimman.entity.ShareLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ShareLocationRepository extends JpaRepository<ShareLocationRepository, Integer> {
+public interface ShareLocationRepository extends JpaRepository<ShareLocation, Integer> {
 }
--- src/main/java/org/devkirby/hanimman/repository/TogetherLocationRepository.java
+++ src/main/java/org/devkirby/hanimman/repository/TogetherLocationRepository.java
@@ -1,8 +1,9 @@
 package org.devkirby.hanimman.repository;
 
+import org.devkirby.hanimman.entity.TogetherLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TogetherLocationRepository extends JpaRepository<TogetherLocationRepository, Integer> {
+public interface TogetherLocationRepository extends JpaRepository<TogetherLocation, Integer> {
 }
