53d34a49f265b94d50991fb437ccc3d1a461770d
nechya00 <122517224+Saeng-ah@users.noreply.github.com>
2024-12-03 20:27:56 +0900

    나눠요 같이가요 primaryAddressId 임시 수정

--- src/main/java/org/devkirby/hanimman/service/ShareService.java
+++ src/main/java/org/devkirby/hanimman/service/ShareService.java
@@ -2,7 +2,6 @@ package org.devkirby.hanimman.service;
 
 import org.devkirby.hanimman.config.CustomUserDetails;
 import org.devkirby.hanimman.dto.ShareDTO;
-import org.devkirby.hanimman.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -10,7 +9,7 @@ import java.io.File;
 import java.io.IOException;
 
 public interface ShareService {
-    void create(ShareDTO shareDTO) throws IOException;
+    void create(ShareDTO shareDTO, String primaryAddressId) throws IOException;
     ShareDTO read(Integer id, CustomUserDetails loginUser);
     void update(ShareDTO shareDTO) throws IOException;
     void delete(Integer id);
