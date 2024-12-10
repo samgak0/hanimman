11d1be9ebf9458be50d1a34788a8cb10fd903032
sonamuyaz <musiwngud@naver.com>
2024-12-10 09:50:01 +0900

    UserAdddressRepository 주석

--- src/main/java/org/devkirby/hanimman/repository/UserAddressRepository.java
+++ src/main/java/org/devkirby/hanimman/repository/UserAddressRepository.java
@@ -4,9 +4,15 @@ import org.devkirby.hanimman.entity.UserAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface UserAddressRepository extends JpaRepository<UserAddress, Integer> {
-    Optional<UserAddress> findByUserId(Integer userId);
+    //특정 사용자 ID에 해당하는 단일 주소
+    UserAddress findByUserId(Integer userId);
+
+    //특정 사용자 ID에 해당하느 모든 주소
+    List<UserAddress> findAllByUserId(Integer userId);
+
+
 }
