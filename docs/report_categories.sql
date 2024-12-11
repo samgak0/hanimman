cd71ef6333ecbfaf341d78e651223a24faded556
p614080 <p614080@naver.com>
2024-12-11 16:47:39 +0900

    report카테고리 수정

--- docs/report_categories.sql
+++ docs/report_categories.sql
@@ -1,7 +1,7 @@
-INSERT INTO report_category (name, manager_id) VALUES ('욕설/비방', 1);
-INSERT INTO report_category (name, manager_id) VALUES ('거래 금지 물품', 1);
-INSERT INTO report_category (name, manager_id) VALUES ('전문 판매업자', 1);
-INSERT INTO report_category (name, manager_id) VALUES ('거래 중 분쟁 발생', 1);
-INSERT INTO report_category (name, manager_id) VALUES ('사기', 1);
-INSERT INTO report_category (name, manager_id) VALUES ('중복 및 도배', 1);
-INSERT INTO report_category (name, manager_id) VALUES ('기타 부적절한 행위', 1);
+INSERT INTO report_categories (name, manager_id) VALUES ('욕설/비방', 1);
+INSERT INTO report_categories (name, manager_id) VALUES ('거래 금지 물품', 1);
+INSERT INTO report_categories (name, manager_id) VALUES ('전문 판매업자', 1);
+INSERT INTO report_categories (name, manager_id) VALUES ('거래 중 분쟁 발생', 1);
+INSERT INTO report_categories (name, manager_id) VALUES ('사기', 1);
+INSERT INTO report_categories (name, manager_id) VALUES ('중복 및 도배', 1);
+INSERT INTO report_categories (name, manager_id) VALUES ('기타 부적절한 행위', 1);
